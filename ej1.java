975d05c81450911d696e3583c218f2700da9c6ee
Deyna28 <deynasanchezf@gmail.com>
2024-12-04 17:20:38 +0100

    atributos-y-constructor

--- ej1.java
+++ ej1.java
@@ -1,22 +1,16 @@
 package ejercicio.depuracionDSF;
 import java.util.*;
 public class ej1 {
-	/*
-	* Programa que lee números hasta que se lee un negativo
-	*  y muestra la suma de los números leídos
-	*/
+private int suma, num;
+	
+	public Ejemplo1While() {
+		this.suma = 0;
+		this.num = 0;
+	}
 	 public static void main(String[] args) {
 	 int suma = 0, num;
-	 int contador=0;
+	 
 	 Scanner sc = new Scanner(System.in);
-	 System.out.print("Introduzca un número: ");
-	 num = sc.nextInt();
-	 while (num>= 0){
-	 suma = suma + num;
-	 contador++;
-	 System.out.print("Introduzca un número: ");
-	 num = sc.nextInt();
-	 }
-	 System.out.println("La suma es: " + suma );
+	 
 	 }
 	}
