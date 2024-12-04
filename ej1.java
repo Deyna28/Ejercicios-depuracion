55cf2608d796356d9f8be11eb2831f2be903af5a
Deyna28 <deynasanchezf@gmail.com>
2024-12-04 17:23:48 +0100

    cambio-a-numero

--- ej1.java
+++ ej1.java
@@ -1,16 +1,15 @@
 package ejercicio.depuracionDSF;
 import java.util.*;
 public class ej1 {
-private int suma, num;
+private int suma, numero;
 	
+
 	public Ejemplo1While() {
 		this.suma = 0;
-		this.num = 0;
+		this.numero = 0;
+		
 	}
 	 public static void main(String[] args) {
-	 int suma = 0, num;
-	 
-	 Scanner sc = new Scanner(System.in);
 	 
 	 }
 	}
